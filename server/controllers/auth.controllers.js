a3b97654b3640b1c743ee519b2bdd241beac0758
Liubre82 <liubre82@students.rowan.edu>
2023-11-14 21:51:22 -0500

    created middleware and function to handle possible errors

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,7 +1,8 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
+import { errorHandler } from '../utils/error.js'
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
     const hashedPassword = bcrypt.hashSync(password, 10) //Auto-gen a salt and hashes the pw
     const newUser = new User({ username, email, password: hashedPassword })
@@ -9,8 +10,7 @@ export const signup = async (req, res) => {
         await newUser.save()
         res.status(201).json('User created successfully')
     } catch(err) {
-        //if the inputted username n email is duplicated, it will throw an error. err.message returns the msg created by express displaying what was the error.
-        res.status(500).json(err.message)
+        next(err)
     }
 
 }
---
+++ server/utils/error.js
@@ -0,0 +1,8 @@
+//errorHandler method creates our own error object and sets our own error status code and error message into the obj.
+
+export const errorHandler = (statusCode, message) => {
+    const error = new Error()
+    error.statusCode = statusCode
+    error.message = message
+    return error
+}
