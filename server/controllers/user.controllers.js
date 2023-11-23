12371babf8b15cdf86c4cef0e49366a9c7299f82
Liubre82 <liubre82@students.rowan.edu>
2023-11-22 19:32:32 -0500

    created userId listing route

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js"
+import Listing from '../models/listing.model.js'
 import { errorHandler } from "../utils/error.js"
 import bcryptjs from 'bcryptjs'
 
@@ -9,10 +10,10 @@ export const test = (req, res) => {
 }
 
 export const updateUser = async (req, res, next) => {
-    const { id } = req.params
+    const { userId } = req.params
     const { username, email, accountImage } = req.body
     //req.user was a property that was passed into the req obj from the previous middleware, verifyUToken
-    if (req.user.id !== id) return next(errorHandler(401, "You can only update your own account!"))
+    if (req.user.id !== userId) return next(errorHandler(401, "You can only update your own account!"))
     try {
         //hash the new password IF the user inputted a new password to be updated
         if (req.body.password) {
@@ -20,15 +21,15 @@ export const updateUser = async (req, res, next) => {
         }
 
         //find the user from the model with the id, and set every property to the 
-        const updateUser = await User.findByIdAndUpdate(id, {
+        const updateUser = await User.findByIdAndUpdate(userId, {
             //$set operator will check for us if the input was changed, if there was no change. for ex: if the user did not input anything in the email inputbox, then $set will ignore that input and it retains its original value. 
             //always specify each indvidual property, passing req.body can cause security issues where users can add their own properties like say in a api tester and just pass fields  that are technically not allowed but since its in the req.body it will get passed in.
             $set: {
                 username, email, password: req.body.password, accountImage
             }
-        }, {new: true})
+        }, { new: true })
 
-        const {password, ...otherDetails} = updateUser._doc;
+        const { password, ...otherDetails } = updateUser._doc;
         res.status(200).json(otherDetails)
     } catch (err) {
         next(err)
@@ -36,14 +37,29 @@ export const updateUser = async (req, res, next) => {
 }
 
 export const deleteUser = async (req, res, next) => {
-    const { id } = req.params
+    const { userId } = req.params
     //req.user is a property that was added to the req obj in the previous middleware. and it stores info on the current user that is logged in.
-    if(req.user.id !== id) return next(errorHandler(401, 'You can only delete your own account!'))
+    if (req.user.id !== userId) return next(errorHandler(401, 'You can only delete your own account!'))
     try {
-        const user = await User.findByIdAndDelete(id)
+        const user = await User.findByIdAndDelete(userId)
         res.clearCookie('access_token')
         res.status(200).json('User has been deleted...')
     } catch (err) {
         next(err)
+    }
+}
+
+export const getUserListing = async (req, res, next) => {
+    const { userId } = req.params
+    if (req.user.id === userId) {
+        try {
+            //every listing created has a userId attatch to it, indicating which user created the listing. to find all the listings of the current logged in user just search the Listing collection for the userId in the 'userRef' field
+            const listings = await Listing.find({ userRef: userId })
+            res.status(200).json(listings)
+        } catch (err) {
+            next(err)
+        }
+    } else {
+        return next(errorHandler(401, 'You can only delete your own account!'))
     }
 }
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,12 +1,14 @@
 import express from 'express'
-import {test, updateUser, deleteUser} from '../controllers/user.controllers.js'
+import {test, updateUser, deleteUser, getUserListing} from '../controllers/user.controllers.js'
 import { verifyToken } from '../utils/verifyUser.js'
 const router = express.Router()
 
 router.get('/test', test)
 
-router.post('/update/:id', verifyToken ,updateUser)
+router.post('/update/:userId', verifyToken ,updateUser)
 
-router.delete('/delete/:id', verifyToken, deleteUser)
+router.delete('/delete/:userId', verifyToken, deleteUser)
+
+router.get('/listings/:userId', verifyToken, getUserListing)
 
 export default router
