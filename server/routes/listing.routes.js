3aeb321ec372a9f0ff00f858d62b1e6bf912a95c
Liubre82 <liubre82@students.rowan.edu>
2023-11-28 21:42:30 -0500

    created search route

--- server/routes/listing.routes.js
+++ server/routes/listing.routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const router = express.Router()
-import {createListing, deleteListing, editListing, getListing} from '../controllers/listing.controllers.js'
+import {createListing, deleteListing, editListing, getListing, getSearchListings} from '../controllers/listing.controllers.js'
 import { verifyToken } from '../utils/verifyUser.js'
 
 router.post('/create', verifyToken, createListing)
@@ -10,5 +10,7 @@ router.delete('/delete/:listingId', verifyToken, deleteListing)
 router.post('/edit/:listingId', verifyToken, editListing)
 
 router.get('/getList/:listingId', getListing)
+
+router.get('/getList', getSearchListings)
 
 export default router
